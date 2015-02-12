534018a55fe896db776174054fcc9c5f55d2232c
Trevor W. Currie <trevor.w.currie@gmail.com>
2015-02-11 19:50:12 -0800

    Provided doc strings, methods close to final form

--- makeTextFiles.py
+++ makeTextFiles.py
@@ -14,6 +14,7 @@ url = re.compile(r"(https://[^ ]+.jpg)")
 
 class TextMessage(object):
     def __init__(self,time,sender,message=None,picture=None)
+        """Set up text message with time, sender, and a message or a picture"""
         self.time = datetime.strptime(time,"%Y %b %d %H:%M:%S")
         
         if sender == 'Me' or sender == '+16267204969':
@@ -24,44 +25,63 @@ class TextMessage(object):
         self.picture = picture
         
         if picture is not None:
-            download_picture(picture)
+            try:
+                download_picture(picture)
+            except ReferenceError:
+                self.picture = None
 
     def __str__(self):
+        """Prints out text message as sender and message"""
         return self.sender, self.message
 
     def download_picture(self):
+        """Downloads picture from given link"""
         picture_directory = '~/Projects/TextsBook/textFiles/pictures/'
         file_name = picture_directory + \
                 strftime(self.time,"%Y_%b_%d_%H_%M_%S") +\
                 + ".jpg"
-        with open(file_name, 'wb') as f:
-            c = pycurl.Curl()
-            c.setopt(c.URL, self.picture)
-            c.setopt(c.WRITEDATA, f)
-            c.perform()
-            c.close()
+        try:
+            with open(file_name, 'wb') as f:
+                c = pycurl.Curl()
+                c.setopt(c.URL, self.picture)
+                c.setopt(c.WRITEDATA, f)
+                c.perform()
+                c.close()
+        except:
+            raise ReferenceError
 
-    def print_to_latex_file(self,file_name,right):
-        with open(file_name,'a') as messages_file:
-            if right == True: 
-                messages_file.write("\quoteRight{\n")
-            else:
-                messages_file.write("\quoteLeft{\n")
 
-            if self.picture is not None:
-                messages_file.write("\includegraphics[\maxWidth]{")
-                messages_file.write(self.picture)
-                messages_file.write("}")
-            elif self.message is not None:
-                messages_file.write(self.message)
-            
-            messages_file.write("\n}{")
-            messages_file.write(self.time.strftime("%d %b %Y %I:%M:%S%p"))
-            messages_file.write("}\n")
+    def print_to_latex_file(self,file_name,print_right):
+        """Prints message to a tex file at file_name"""
+        if os.path.exists(file_name):
+            start = "\n"
+        else:
+            start = "\chapter{"+strftime("%B",self.time)+"}\n"
+        try:
+            with open(file_name,'a') as messages_file:
+                messages_file.write(start)
+                if print_right == True: 
+                    messages_file.write("\quoteRight{\n")
+                else:
+                    messages_file.write("\quoteLeft{\n")
 
+                if self.picture is not None:
+                    messages_file.write("\includegraphics[\maxWidth]{")
+                    messages_file.write(self.picture)
+                    messages_file.write("}")
+                elif self.message is not None:
+                    messages_file.write(self.message)
+                
+                messages_file.write("\n}{")
+                messages_file.write(self.time.strftime("%d %b %Y %I:%M:%S%p"))
+                messages_file.write("}\n")
+        except:
+            print "Error in printing to file: "+messages_file
 
 class Conversation(object)
+    """Stores list of text messages"""
     def __init__(self,conversation_file):
+        """Initialize conversation by reading text message files"""
         self.text_messages = []
         if os.path.exists(conversation_file):
             with open(conversation_file,'r') as conversation:
@@ -79,26 +99,56 @@ class Conversation(object)
                             split_message = message.split(picture_link)
                             message = split_message[1]
                             self.text_messages.append(\
-                                    TextMessage(time, sender, message = split_message[0]))
+                                    TextMessage(time, sender, \
+                                    message = split_message[0]))
                             self.text_messages.append(\
-                                    TextMessage(time, sender, picture = picture_link))
+                                    TextMessage(time, sender, \
+                                    picture = picture_link))
                     else:
                         self.text_messages.append(\
-                                 TextMessage(time, sender, message=message))
+                                 TextMessage(time, sender, \
+                                 message=message))
 
         self.sort_messages()
     
     def __add__(self,other):
+        """ Adds two conversation objects together by combining 
+            their lists"""
+        
         if type(other) == type(self):
             raise TypeError
         else:
             self.messages += other.messages
             self.sort_messages()
     
+    def get_time_of_first_message(self):
+        """ Returns the earliest time in the conversation"""
+        
+        self.sort_messages()
+        return self.messages[0].time
+
     def sort_messages(self):
+        """ Sorts messages by timestamp """
+        
         self.messages.sort(key=lambda text: text.time)#, reverse=True)
 
+    def print_messages_to_latex(self, user):
+        """ Prints the latex format of 
+        each message in conversation."""
+        
+        conversation_directory = "~/Projects/TextsBook/textFiles/"
+        conversation_file = strftime("%B",self.get_time_of_first_message())+".tex"
+        for message in self.messages:
+            if message.sender == user:
+                message.print_to_latex_file(conversation_file,True)
+            else:
+                message.print_to_latex_file(conversation_file,False)
 
 if  __name__ =='__main__':
-    current_directory = os.path.abspath(os.curdir)	
+    texts_directory = '~/Projects/Voice/Texts/'
+    person = 'Klaudia Helena Zarako-Zarakowski'
+    
+    #for root,path,files in os.walk(texts_directory+person+'/'):
+         
+
 
